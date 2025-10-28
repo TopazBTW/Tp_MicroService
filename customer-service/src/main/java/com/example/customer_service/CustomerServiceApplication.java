f67eab5541fff53f8587f32cc85b79f2a560ef35
TopazBTW <berserkmj23@gmail.com>
2025-10-28 01:27:32 +0100

    release me

--- customer-service/src/main/java/com/example/customer_service/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/customer_service/CustomerServiceApplication.java
@@ -1,7 +1,11 @@
 package com.example.customer_service;
 
+import com.example.customer_service.entities.Customer;
+import com.example.customer_service.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -9,5 +13,21 @@ public class CustomerServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
-
+@Bean
+CommandLineRunner init(CustomerRepository customerRepository) {
+        return args -> {
+            Customer customer1 = Customer.builder()
+                    .firstName("Taha")
+                    .lastName("Kumar")
+                    .email("Taha@gmail.com")
+                    .build();
+            customerRepository.save(customer1);
+            Customer customer2 = Customer.builder()
+                    .firstName("Houbid")
+                    .lastName("Kumar")
+                    .email("Houbid@gmail.com")
+                    .build();
+            customerRepository.save(customer2);
+        };
+}
 }
